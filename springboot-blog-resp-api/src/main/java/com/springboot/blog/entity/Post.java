da276a9b2e3313a022e286e9a708127806850147
tahirkhalifa121 <taher.khalifa121@gmail.com>
2023-03-06 19:25:17 +0530

    post entity,repo

---
+++ springboot-blog-resp-api/src/main/java/com/springboot/blog/entity/Post.java
@@ -0,0 +1,63 @@
+package com.springboot.blog.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table( name = "blog_posts",uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})})
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "title",nullable = false)
+	private String title;
+	@Column(name = "description",nullable = false)
+	private String description;
+	@Column(name = "content",nullable = false)
+	private String content;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public Post(Long id, String title, String description, String content) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.content = content;
+	}
+	
+	
+	
+	
+	
+}
